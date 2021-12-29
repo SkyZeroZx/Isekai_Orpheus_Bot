710a6c03c70fd5a4b4e34b03ae0a3ff6700a53b3
SkyZeroZx <73321943+SkyZeroZx@users.noreply.github.com>
2021-12-29 02:50:06 -0500

    Update index.ts

--- API/src/routes/index.ts
+++ API/src/routes/index.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import auth from './auth';
+import tramite from "./tramite";
 import user from './user';
 
 const routes = Router();
 
 routes.use('/auth',auth);
 routes.use('/users',user);
-routes.use('/tramite',user);
+routes.use('/tramite',tramite);
 export default routes;
