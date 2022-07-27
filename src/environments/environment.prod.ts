3a9d2c64e33a02879adc4c055d8dc4d04bebd737
SkyZeroZx <73321943+SkyZeroZx@users.noreply.github.com>
2022-07-26 19:43:36 -0500

    Update Heroku API host

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,8 @@ export const environment = {
   production: true,
   VAPID_PUBLIC_KEY:
     "BP8RV6DeHoKcmD_bi3JyyWD7cEjECZbdg6jpq3kftSOLsb6r5x4LmjaTi4khyzyz14mmYg9noS8i6GiiSi0_GHA",
-  API_URL: "https://whatsapp-bot-isekai.herokuapp.com",
+ // API_URL: "https://whatsapp-bot-isekai.herokuapp.com",
+ API_URL: "https://isekai-orpheus-bot-backend-exp.herokuapp.com",
   //  API_URL: "http://localhost:3000",
   URL_TRACK: "https://skyzerozx.000webhostapp.com/#/tracking",
 };
